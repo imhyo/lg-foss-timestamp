547235b22ded02a59b11ccaccd33554703374add
Im, HyoJun <hyojun.im@gmail.com>
2015-01-01 16:26:46 +0900

    This implements the basic features of the application:   - front page with the text box and submit button.   - Data store to record working history.   - Clinking the 'Check-in' button adds a new log into the data store.

--- timestamp.py
+++ timestamp.py
@@ -1,10 +1,55 @@
+import os
+import cgi
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions = ['jinja2.ext.autoescape'],
+	autoescape = True)
+
+# We set a parent key on the 'Username' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+def user_key(user):
+	return ndb.Key('Username', user.user_id())
+	
+class Timestamp(ndb.Model):
+	"""Models an individual Timestamp entry."""
+	start = ndb.DateTimeProperty(auto_now_add=True)
+	finish = ndb.DateTimeProperty(auto_now_add=False)
+	content = ndb.StringProperty(indexed=False)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+		user = users.get_current_user()
+		
+		timestamps_query = Timestamp.query(
+			ancestor = user_key(user)).order(-Timestamp.start)
+		timestamps = timestamps_query.fetch(5)
+		
+		template_values = {
+			'timestamps': timestamps,
+		}
+		
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+
+class Checkin(webapp2.RequestHandler):
+	def post(self):
+		user = users.get_current_user();
+		timestamp = Timestamp(parent=user_key(user))
+		timestamp.put()
+		self.redirect('/')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+	('/checkin', Checkin),
 ], debug=True)
